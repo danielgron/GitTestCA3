af6e2654b034bcac3c0c6ef121f0e6faa8883d41
danielgron <danielgronbjerg@gmail.com>
2016-11-21 10:40:28 +0100

    Putting ApplicationConfig up for Travis

---
+++ src/main/java/rest/ApplicationConfig.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author danie
+ */
+@javax.ws.rs.ApplicationPath("api")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(httpErrors.GenericExceptionMapper.class);
+        resources.add(httpErrors.NotFoundExceptionMapper.class);
+        resources.add(rest.Admin.class);
+        resources.add(rest.All.class);
+        resources.add(rest.Coordinator.class);
+        resources.add(rest.DepartmentAdmin.class);
+        resources.add(rest.EventService.class);
+        resources.add(rest.RequestService.class);
+        resources.add(rest.User.class);
+        resources.add(rest.WatchService.class);
+        resources.add(security.JWTAuthenticationFilter.class);
+        resources.add(security.Login.class);
+        resources.add(security.NotAuthorizedExceptionMapper.class);
+        resources.add(security.RolesAllowedFilter.class);
+    }
+    
+}
