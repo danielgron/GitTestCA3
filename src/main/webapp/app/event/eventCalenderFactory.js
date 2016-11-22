9d2bf3366cdaecd18be0c4bd6e4e648d82bd3d51
Danielh21 <danielhollmann@hotmail.com>
2016-11-22 10:31:52 +0100

    Issue #2 - Frontend, Gets info about clicked event

--- src/main/webapp/app/event/eventCalenderFactory.js
+++ src/main/webapp/app/event/eventCalenderFactory.js
@@ -4,20 +4,25 @@ angular.module('myApp.calendar').factory('calendarFactory', function ($http) {
     var clickedEventid = 0;
 
     var getEvents = function () {
-            return $http.get(urlBase);
-        };
+        return $http.get(urlBase);
+    };
 
-     var getClickedEventid = function () {
-            return clickedEventid;
-        };
+    var getClickedEventid = function () {
+        return clickedEventid;
+    };
 
-       var setClickedEventId = function (id) {
-            clickedEventid = id;
-        };
-        
+    var setClickedEventId = function (id) {
+        clickedEventid = id;
+    };
+
+    var getSingleEvent = function (id) {
+      return $http.get(urlBase + "/" + id);
+    };
+
     return {
         getEvents,
         getClickedEventid,
-        setClickedEventId
+        setClickedEventId,
+        getSingleEvent
     };
 });
--- src/main/webapp/app/event/singleevent.js
+++ src/main/webapp/app/event/singleevent.js
@@ -1,15 +1,26 @@
 angular.module('myApp.singleevent', ['ngRoute'])
         .config(['$routeProvider', function ($routeProvider) {
                 $routeProvider.when('/singleevent', {
-                    templateUrl: "app/event/template/singleeventview.html",
-                    controller: "SingleEvtCtrl"
+                    templateUrl: "app/event/template/singleeventview.html"
+                    
                 });
             }])
         
          .controller('SingleEvtCtrl', ['$scope', '$locale', 'calendarFactory', function ($scope, $locale, calendarFactory) {
                  var self = this;
                  self.eventid = calendarFactory.getClickedEventid();
+                 self.event;
+                 getClickedEvent();
                  
+                 function getClickedEvent(){
+                     calendarFactory.getSingleEvent(self.eventid)
+                             .then(function (response){
+                                 self.event = response.data;
+                     }), function (error){
+                         console.log("Error" + error);
+                     };
+                 }
+                 console.log("stop");
                  
             }]);
                 
