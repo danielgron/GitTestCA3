8f3616a1f9b1a2da3bc194d5efa66bdef41b515b
daeniz <dennis@schmock.eu>
2016-12-05 10:50:27 +0100

    added the stuff from India

---
+++ src/main/webapp/app/requestform/ng-sevices/indexService.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+    angular.module('redCrossApp').service('indexService', ['$http', indexService]);
+
+    function indexService($http) {
+
+        //#region "Constructor"
+        var service = {
+            saveData: saveData
+        }
+        return service;
+        //#endregion
+
+        function saveData(items, onSuccess, onError) {
+            var config = {
+                headers: {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+            }
+            $http.post('http://daeniz.dk/rk/api/demoall', items, config)
+                .success(function (data, status, headers) {
+                    onSuccess(data)
+                })
+                .error(function (data, status, header, config) {
+                    onError(data)
+                });
+        }
+    }
+})();
